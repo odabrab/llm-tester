f8684110e3a47747fad3d2411c7d54221572a95c
Marcio Barbado, Jr. <marcio.barbado@bdslabs.com.br>
2024-09-30 16:35:05 -0300

    Additional sync adjustments between Git and Expo Snack.

--- components/RegisterView.js
+++ components/RegisterView.js
@@ -6,14 +6,29 @@
 //////////////////////////////////////////////////////////////////////
 ////////  TELA DE REGISTRO DE UMA NOVA CONTA
 ////////
-////////  File name:     RegisterFullView.js.
+////////  File name:     RegisterView.js.
 ////////  Author:        Marcio Barbado, Jr.
-////////  Contact:       <marciojunior@hotmail.com>.
+////////  Contact:       <marcio.barbado@bdslabs.com.br>.
 ////////  Place:         S&atilde;o Paulo, Brasil.
 ////////  Copyright (c): 2024.
 ////////  License:       General Public License, version 2.
 ////////
-////////  [LICENSE_SHORT_TEXT].
+////////  This program is free software; you can redistribute it
+////////  and/or modify it under the terms of the GNU General Public
+////////  License as published by the Free Software Foundation;
+////////  either version 2 of the License, or (at your option) any
+////////  later version.
+////////  
+////////  This program is distributed in the hope that it will be
+////////  useful, but WITHOUT ANY WARRANTY; without even the implied
+////////  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+////////  PURPOSE. See the GNU General Public License for more
+////////  details.
+////////  
+////////  You should have received a copy of the GNU General Public
+////////  License along with this program; if not, write to the Free
+////////  Software Foundation, Inc., 59 Temple Place, Suite 330,
+////////  Boston, MA 02111-1307 USA
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
